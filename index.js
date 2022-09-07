c0bfc897240e6b6128bd31bbfb6581aa4cc3b3ad
Kay Bi <kaybi9x@gmail.com>
2022-09-07 12:14:09 +0700

    add

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+const upload = multer();
+const port = 8000;
+app.set('view engine', 'ejs');
+app.set('views','./views');
+const url = require('url');
+const qs = require('qs');
+
+app.get('/create', (req, res) => {
+    res.render('addEmployee');
+});
+const employees = [];
+app.post('/', upload.none(), (req, res) => {
+    if (req.body.id && req.body.name  && req.body.department) {
+        let employee = {
+            id: req.body.id,
+            name: req.body.name,
+            department: req.body.department
+        }
+
+        employees.unshift(employee);
+        res.render('listEmployee', {data: employees})
+    } else {
+        res.render('notFound');
+    }
+})
+
+app.get('/' , (req, res) => {
+    res.render('listEmployee' , {data: employees})
+})
+
+app.get('/delete', (req, res) => {
+    let id = qs.parse(url.parse(req.url).query).id;
+    employees.splice(id - 1, 1);
+    res.render('listEmployee', {data: employees});
+})
+
+app.listen(port, ()=> {
+    console.log(`http://localhost:${port}`)
+})
